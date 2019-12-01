e893aabce76ef92168ac578e13698d32b092c921
Nataliia Husak <nataliy.husak@gmail.com>
2019-12-01 09:57:51 +0200

    01
    02

--- task.js
+++ task.js
@@ -1,40 +1,5 @@
 "use sctict";
 
-// const doc = document.querySelector("body");
-
-// const timer = new CountdownTimer({
-//   selector: "#timer-1",
-//   targetDate: new Date("Jan 01, 2020")
-// });
-
-// /*
-//  * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
-//  * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
-//  */
-// const days = Math.floor(time / (1000 * 60 * 60 * 24));
-
-// /*
-//  * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
-//  * остатка % и делим его на количество миллисекунд в одном часе
-//  * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
-//  */
-// const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-// /*
-//  * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
-//  * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
-//  */
-// const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-// /*
-//  * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
-//  * миллисекунд в одной секунде (1000)
-//  */
-// const secs = Math.floor((time % (1000 * 60)) / 1000);
-
-
-
-
 const refs = {
   days: document.querySelector('span[data-value="days"]'),
   hours: document.querySelector('span[data-value="hours"]'),
@@ -44,8 +9,8 @@ const refs = {
 
 class CountdownTimer {
   constructor({ selector, targetDate }) {
-    this.container = document.querySelector(selector),
-    this.time = targetDate;
+    (this.container = document.querySelector(selector)),
+      (this.time = targetDate);
   }
 
   getTime() {
@@ -60,7 +25,9 @@ class CountdownTimer {
 
   updateClockFace(time) {
     const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-    const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
     const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
     const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
@@ -71,15 +38,13 @@ class CountdownTimer {
   }
 
   pad(value) {
-    return String(value).padStart(2, '0');
+    return String(value).padStart(2, "0");
   }
-};
-
-
+}
 
 const newCountdownTimer = new CountdownTimer({
   selector: "#timer-1",
-  targetDate: new Date("Jan 30, 2020")
+  targetDate: new Date("Jan 01, 2020")
 });
 
 newCountdownTimer.getTime();
